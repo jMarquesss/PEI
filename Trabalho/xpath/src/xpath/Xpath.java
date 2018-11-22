67c323fb5d148f8d56fe1972ea9e6ca74c11f80b
ricardof18 <ricardo_miguel31@hotmail.com>
2018-11-22 16:52:36 +0000

    decimo commit

--- Trabalho/xpath/src/xpath/Xpath.java
+++ Trabalho/xpath/src/xpath/Xpath.java
@@ -19,7 +19,7 @@ public class Xpath {
      */
     public static void main(String[] args) {
 
-        XMLManagement teste = new XMLManagement("C:\\Users\\ricar\\Desktop\\ESTG\\PEI\\Trabalho\\XML\\exerciseXml.xml", "C:\\Users\\ricar\\Desktop\\ESTG\\PEI\\Trabalho\\XSD\\Exercise.xsd");
+        XMLManagement teste = new XMLManagement("C:\\Users\\ricar\\Desktop\\ESTG\\PEI\\Trabalho\\XML\\clienteXml.xml", "C:\\Users\\ricar\\Desktop\\ESTG\\PEI\\Trabalho\\XSD\\Cliente.xsd");
 
         teste.read(true);
         System.out.println(teste.validate(true));
