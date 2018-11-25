2e500a3c99ef3339149d6c3de9f661c8fdfe3948
8170262 <45042385+8170262@users.noreply.github.com>
2018-11-25 17:13:40 +0000

    Z commit

---
+++ PEI_EE_8170200_8170262_8170279/Tester/src/xpath/Xpath.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xpath;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ricar
+ */
+public class Xpath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        XMLManagement teste = new XMLManagement("C:\\Users\\ricar\\Desktop\\ESTG\\PEI\\Trabalho\\XML\\ExerciseXml.xml", "C:\\Users\\ricar\\Desktop\\ESTG\\PEI\\Trabalho\\XSD\\Exercise.xsd");
+
+        teste.read(true);
+        System.out.println(teste.validate(true));
+        /*
+        try {
+
+            System.out.println(XpathEvaluator.applyXpathExpressionToString("/CATALOG/CD[COUNTRY=\"USA\"]", teste.getDocument()));
+        } catch (XpathNoResultsException ex) {
+            Logger.getLogger(Xpath.class.getName()).log(Level.SEVERE, null, ex);
+        }
+*/
+    }
+
+}
